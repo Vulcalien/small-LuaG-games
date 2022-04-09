1aab61f75ad7329ba07cffdb24f238a8ca3353ed
Vulcalien <vulcalien@gmail.com>
2022-04-09 10:41:07 +0200

    Add more information about bodies (vel, acc) and scale

--- trajectory/console-userdata/scripts/main.lua
+++ trajectory/console-userdata/scripts/main.lua
@@ -1,3 +1,22 @@
+function draw_line(x, y, xd, yd, color)
+    local len = math.sqrt(xd * xd + yd * yd)
+
+    if len < 1 then
+        return
+    end
+
+    local sin = yd / len
+    local cos = xd / len
+
+    for i=0,math.floor(len + 0.5) do
+        pix(
+            math.floor(x + cos * i + 0.5),
+            math.floor(y + sin * i + 0.5),
+            color
+        )
+    end
+end
+
 function new_body(name, icon, mass, pos, vel)
     local result = {
         name = name,
@@ -12,7 +31,7 @@ function new_body(name, icon, mass, pos, vel)
             end
             for _,body in ipairs(bodies) do
                 if body == self then
-                    break
+                    goto continue
                 end
 
                 local d = {}
@@ -28,6 +47,7 @@ function new_body(name, icon, mass, pos, vel)
                 for i=0,ndim-1 do
                     self.acc[i] = self.acc[i] + g * d[i] / dist
                 end
+                ::continue::
             end
         end,
         move = function(self)
@@ -41,6 +61,45 @@ function new_body(name, icon, mass, pos, vel)
             local yr = yoff + math.floor(self.pos[1] / scale + 0.5)
 
             spr(icon, xr - 4, yr - 4)
+
+            if gui.body_info >= 1 then
+                do -- write position
+                    local str = ''
+                    for i=0,ndim-1 do
+                        if i ~= 0 then
+                            str = str .. ' '
+                        end
+                        str = str .. math.floor(self.pos[i] + 0.5)
+                    end
+                    write(str, 0xffffff, xr + 4, yr - 4 - font_h)
+                end
+
+                draw_line(xr, yr, self.vel[0], self.vel[1], 0xffff00)
+                draw_line(xr, yr, self.acc[0], self.acc[1], 0xff0000)
+            end
+
+            if gui.body_info >= 2 then
+                do -- write velocity
+                    local str = ''
+                    for i=0,ndim-1 do
+                        if i ~= 0 then
+                            str = str .. ' '
+                        end
+                        str = str .. math.floor(self.vel[i] + 0.5)
+                    end
+                    write(str, 0xffff00, xr + 4, yr - 4)
+                end
+                do -- write acceleration
+                    local str = ''
+                    for i=0,ndim-1 do
+                        if i ~= 0 then
+                            str = str .. ' '
+                        end
+                        str = str .. math.floor(self.acc[i] + 0.5)
+                    end
+                    write(str, 0xff0000, xr + 4, yr - 4 + font_h)
+                end
+            end
         end
     }
 
@@ -57,6 +116,11 @@ end
 
 function init()
     settransparent(0x000000)
+
+    gui = {
+        body_info = 0,
+        generic_info = true
+    }
 
     offset = {}
     offset[0] = 0
@@ -98,6 +162,14 @@ function tick()
     if scale == 0 then
         scale = 1
     end
+
+    -- toggle info
+    if key_pressed(4) then
+        gui.body_info = (gui.body_info + 1) % 3
+    end
+    if key_pressed(5) then
+        gui.generic_info = not gui.generic_info
+    end
 end
 
 function render()
@@ -108,5 +180,9 @@ function render()
 
     for _,body in ipairs(bodies) do
         body:render(xoff, yoff)
+    end
+
+    if gui.generic_info then
+        write("scale: " .. scale, 0xaaaaaa, 1, scr_h - font_h)
     end
 end
